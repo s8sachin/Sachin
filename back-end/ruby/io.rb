9f37ac35d2055af269109e57bc394c3479413e39
Ms.Priyanka Bar <priyankabalasubramanya@gmail.com>
2016-09-28 17:26:50 +0530

    28-sep, ruby and update

---
+++ back-end/ruby/io.rb
@@ -0,0 +1,13 @@
+puts "hi"
+a=10
+b="String"
+puts "a = #{a} "
+putc "#{b}"
+puts "	"
+print "print command"
+puts "Enter new value"
+c= gets.to_i
+d=gets.to_i
+e=c+d
+puts e
+file=File.new('a.txt')
